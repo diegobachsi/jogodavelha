b085f9234a8832ccf10ac2dc55574dfa5bae1c37
Diego Costa Alves <61090568+diegobachsi@users.noreply.github.com>
2023-02-04 12:59:52 -0300

    Update settings.py

--- jogodavelha/settings.py
+++ jogodavelha/settings.py
@@ -15,10 +15,6 @@ from pathlib import Path
 import dj_database_url
 from decouple import config
 
-from django.db.backends.mysql.base import DatabaseWrapper
-
-DatabaseWrapper.data_types['DateTimeField'] = 'datetime' # fix for MySQL 5.5
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
